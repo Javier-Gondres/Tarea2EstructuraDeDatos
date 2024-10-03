04c84f7a39eebae77ac4b518e04c935e6402902c
Javier-Gondres <jegd0001@ce.pucmm.edu.do>
2024-10-03 14:34:19 -0400

    Creacion del arreglo para probar commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int[] arreglo = new int[8];
+        arreglo[0] = 1;
+        arreglo[2] = 2;
+        arreglo[3] = 3;
+        arreglo[4] = 4;
+        arreglo[5] = 5;
+        arreglo[6] = 6;
+        arreglo[7] = 7;
+    }
+
+    public void T(){
+
+    }
+}
